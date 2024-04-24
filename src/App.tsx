29ef5dbe77023208c3d57fbab1cf08d04f1337b7
kene Ojukwu <keneojukwu@kenes-MacBook-Pro.local>
2024-04-23 22:05:04 -0400

    added signup form

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 
 import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Layout from './layouts/Layout'
+import Register from './Register'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout><p>Home Page</p></Layout>} />
         <Route path="/search" element={<Layout><p>Search Page</p></Layout>} />
+        <Route path="/register" element={<Layout><Register/></Layout>} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
